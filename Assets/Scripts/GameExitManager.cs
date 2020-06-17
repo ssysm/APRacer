d7dd0544ed3018e0eb085da4366bf7486f4d1412
ssysm <contact@theeditorstudio.com>
2020-06-16 23:41:49 -0400

    update phy

--- Assets/Scripts/GameExitManager.cs
+++ Assets/Scripts/GameExitManager.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class GameExitManager : MonoBehaviour
 {
-    public GameObject QuitUI;
+    public GameObject QuitUI,LapTimeManager,CarControl,AICar;
     public AudioSource BGMAudio;
     public void Dismiss(){
         QuitUI.gameObject.SetActive(false);
+        LapTimeManager.gameObject.SetActive(true);
         BGMAudio.UnPause();
+        CarControl.gameObject.SetActive(true);
+        AICar.gameObject.SetActive(true);
     }
 
     public void Exit(){
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
 
-    public GameObject ExitUI;
+    public GameObject ExitUI,LapTimeManager, CarControl,AICar;
     public AudioSource BGMAudio;
+    public AudioListener GAudioListener;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey("escape"))
         {
             BGMAudio.Pause();
+            LapTimeManager.gameObject.SetActive(false);
             ExitUI.gameObject.SetActive(true);
+            CarControl.gameObject.SetActive(false);
+            AICar.gameObject.SetActive(false);
         }
     }
 }
